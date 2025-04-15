69e228edbaad8c98f2d687b6a504870f9e1bc8ca
SpitfireGG <raambasnet02@gmail.com>
2025-04-15 22:10:46 +0545

    sets

---
+++ ch02/ch02.nix
@@ -0,0 +1,30 @@
+#sets
+
+/*
+  let
+    person = {
+      name = "scoop";
+      age = 10;
+      address = "kathmandu, Nepal";
+    };
+  in
+  person.address
+*/
+
+# run with nix eval --file ch02.nix
+
+# this syntax can be seen in the nix store as well as when you are configuring configration.nix or other nix files
+
+let
+  services = {
+    name = "ssh";
+    description = "secure shell for securely connecting to remote servers";
+    # this is a nested set attribute
+    settings = {
+      enable = true;
+      params = "<some parameters>";
+      auto_enable_ssh_daemon = false;
+    };
+  };
+in
+services.settings.auto_enable_ssh_daemon
