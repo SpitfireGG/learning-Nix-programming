346df4fc6010bce03dd7e2a63163192c1ff404ec
SpitfireGG <raambasnet02@gmail.com>
2025-04-17 15:40:23 +0545

    added more details

--- ch02/ch02.nix
+++ ch02/ch02.nix
@@ -2,40 +2,42 @@
 
 # NOTE: comment one of the following codes and run for yourself to find the output and tinker the code to see what happens
 
-  let
-    person = {
-      name = "scoop";
-      age = 10;
-      address = "kathmandu, Nepal";
-    };
-  in
-  person.address
+# comment or uncomment one of the section and play around until you feel confident and move onto the next section
 
+# section 1
+let
+  person = {
+    name = "scoop";
+    age = 10;
+    address = "kathmandu, Nepal";
+  };
+in
+person.address
+  # run with nix eval --file ch02.nix
 
-# run with nix eval --file ch02.nix
-
-# this syntax can be seen in the nix store as well as when you are configuring configration.nix or other nix files
-
-
-  let
-    services = {
-      name = "ssh";
-      description = "secure shell for securely connecting to remote servers";
-      # this is a nested set attribute
-      settings = {
-        enable = true;
-        params = "<some parameters>";
-        auto_enable_ssh_daemon = false;
+  # section 2
+  /*
+    let
+      services = {
+        name = "ssh";
+        description = "secure shell for securely connecting to remote servers";
+        # this is a nested set attribute
+        settings = {
+          enable = true;
+          params = "<some parameters>";
+          auto_enable_ssh_daemon = false;
+        };
       };
-    };
-  in
-  services.settings.auto_enable_ssh_daemon
+    in
+    services.settings.auto_enable_ssh_daemon
+  */
 
+  # section 3
+  # Recursive sets
+  rec {
+    x = 1;
+    y = x + 1;
+    z = y + 1;
+  }
 
-# Recursive sets
-
-rec {
-  x = 1;
-  y = x + 1;
-  z = y + 1;
-}
+#section 4
