31199779201f39d56a17eaf00698bab95139facc
SpitfireGG <raambasnet02@gmail.com>
2025-04-19 13:04:12 +0545

    add: more detailed guides on lazy evaluation

--- ch05/ch05.nix
+++ ch05/ch05.nix
@@ -1,9 +1,20 @@
-# nix only evaluates expression when needed ( i.e why it is called lazily evaluated ) & avoids unnecessary computations
-# The unevaluated nix expressions are stored in thunks and need to be forcefully evaluated
+## Lazy Evalutions
+# =============================
+
+### learn more about the nix's lazy evaluation deeply , checkout: https://nixcademy.com/posts/what-you-need-to-know-about-laziness/
+
+/*
+  nix only evaluates expression when needed ( i.e why it is called lazily evaluated ) & avoids unnecessary computations
+  this means that the value of variables is not computed when they are created — it is computed when they are referenced
+  The unevaluated nix expressions are stored in thunks and need to be forcefully evaluated
+*/
 
 let
   expensive_eval = builtins.trace "computing expensive" (100000 * 1000000);
   cheap_eval = builtins.trace "computing cheap" (10 * 10);
+
+  # we created two variabeles expensive and cheap_eval but their values are not computed when they are created but when they are referenced their values will be computed [  This can be throught of a parameters function that when called evaluates the expression   ]
+  # we use the in block to reference the created variables and then compute them
 
   # you can also use the builtins.seq function to evalute an expression strictly
   expensive = builtins.trace "Running expensive computation" (
@@ -18,7 +29,8 @@ let
 in
 {
 
-  # Option 1 : evaluate multiple expressions, we have to use the inherit attribute ( Selectively evaluates the expressions )
+  # Option 1 : to evaluate multiple expressions, we have to use the inherit attribute ( which will selectively evaluate the expressions )
+  # inherit  brings the attributes from  the local scope to the current scope
 
   inherit
     cheap_eval
@@ -31,4 +43,5 @@ in
   forced = force-all; # strictly evaluates the expression
 
 }
-# expensive_eval # this will only compute the  value for expensive_eval & not for cheap_eval
+# the following will only compute the  value for expensive_eval & not for cheap_eval
+# expensive_eval
