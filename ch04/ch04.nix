2c56794b491cbbf3d4e649a2281111e4507ffd55
SpitfireGG <raambasnet02@gmail.com>
2025-04-21 15:38:42 +0545

    add_(docs)nix: variadic & ellipse`@`

--- ch04/ch04.nix
+++ ch04/ch04.nix
@@ -1,11 +1,39 @@
-# comprehensive guide to functions in nix
+## comprehensive guide to functions in nix
+# =============================================
 
-# one thing i noticed is that the output is sometimes random like when working with maps or something , i think this is due to the lazy evaluation of the nix programming language but if you can find more about it if you dive deeper into the lannguage specifications and implementation
+/*
+  one thing i noticed is that the output is sometimes random like when working with maps or something , i think this is due to the lazy evaluation of the nix programming language but if you can find more about it if you dive deeper into the lannguage specifications and implementation
+
+   Working with
+       recursions
+   or `rec`usions is covered in the same directory under ./ch04Extended_2.nix
+*/
 
 let
 
   double = x: x * 2; # single parameter function
   add = x: y: x + y; # double parameter function
+
+  ## ellipse (@-pattern), understand that the ...  is the ellipse
+  someFn =
+    args@
+    # args@ binds the entire input set to args
+    {
+      a,
+      b,
+      c,
+      ...
+    }:
+    a + b + c + args.d;
+
+  # some more examples:
+
+  useDefault =
+    {
+      a ? "default",
+      ...
+    }@args:
+    a + args.b;
 
   # you can also set the default values to the parameters
   SumWithDefaults =
@@ -14,6 +42,7 @@ let
       b ? 0,
     }:
     a + b;
+
   # variadic params
   IgnoreExtras =
     {
@@ -22,6 +51,7 @@ let
       ...
     }:
     a + b;
+
   # Attribute set params ( Attributes are key-value pairs just like map in programming language like GO)
   setAttr = { a, b }: a + b;
 
@@ -33,7 +63,7 @@ let
     f: g: x:
     f (g x);
 
-  # advanced operations
+  ## advanced operations
 
   factorial = n: if n == 0 then 1 else n * factorial (n - 1);
 
@@ -41,7 +71,6 @@ let
   debugMap = f: xs: map (x: builtins.trace "processing ${toString x}" (f x)) xs;
 
   # an example combining everything covered in this chapter
-
   mkUser =
     {
       name ? "kenzo",
@@ -49,7 +78,6 @@ let
       ...
     }:
     {
-
       username = name;
       useage = age;
       EligibleToVote = age >= 18;
@@ -59,17 +87,29 @@ in
 {
   doubles = double 10;
   add_two = add 10 20;
+
+  sF = someFn {
+    a = "a";
+    b = "b";
+    c = "c";
+    d = "d";
+  };
+  useDef = useDefault;
+
   def = SumWithDefaults { a = 6; }; # b is 0
+
   ignored = IgnoreExtras {
     a = 10;
     b = 20;
     c = 30;
     d = 40;
   }; # here c and d are ignored ( you might have seen this in flake.nix or other nix files )
+
   setAttrRes = setAttr {
     a = 1;
     b = 2;
   };
+
   HigherOrderfnRes = HigherOrderfn double 5;
 
   composefnRes = composefn double (x: x + 1) 5;
