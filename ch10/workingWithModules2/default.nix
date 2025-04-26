84d4bfbad0a9f313427a869d4e44b9de6e159a05
SpitfireGG <raambasnet02@gmail.com>
2025-04-26 10:25:03 +0545

    add: evaluation method/ extra docs

--- ch10/workingWithModules2/default.nix
+++ ch10/workingWithModules2/default.nix
@@ -1,12 +1,21 @@
-let
-  pkgs = import <nixpkgs> { };
-  finalEval = pkgs.lib.evalModules {
-    modules = [
-      ./options.nix
-      ./config.nix
-      ./assertions.nix
+{ pkgs, ... }:
+(pkgs.lib.evalModules {
+  modules = [
+    ./options.nix
+    ./config.nix
+    ./assertions.nix
 
-      { config = { enable = false; }; }
-    ];
-  };
-in finalEval.config
+    { config = { enable = true; }; }
+  ];
+}).config
+
+/* the evaluation can be done by the command :
+
+    1.  nix eval --file default.nix --apply 'x: x { pkgs = import <nixpkgs> {};}' --json  | jq .
+
+   this  will import nipkgs using a  function
+
+   2.  nix eval --expr  '(import ./default.nix ) { pkgs = import <nixpkgs> {}; }' --json --impure  | jq
+
+   breaking the expression,  we have imported the module default.nix ( which was not necessary and can be removed ) , import the nixpkgs as default.nix needs it  as defined in the attribute set top-level, pass it to json and then --impure flag as nixpkgs looksup in the $NIX_PATH to let it search the relevant path  as pure evaluation does not allow it to happen and then pretty print it with jq
+*/
