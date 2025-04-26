3e60212e6fbeab0e25c5ba2325ea0986973c169f
SpitfireGG <raambasnet02@gmail.com>
2025-04-26 10:21:03 +0545

    add: assertions & warnings

--- ch10/workingWithModules2/config.nix
+++ ch10/workingWithModules2/config.nix
@@ -2,18 +2,32 @@
 
 let cfg = config;
 
-in with lib  ; {  # using the with for using the helper functions like mkMerge
+in with lib; { # using with for using the helper functions like mkMerge
   config = mkMerge [
-    (mkIf cfg.enable { 
-      name = "working with file module";
+    (mkIf cfg.enable {
+      name = "enable/disable config";
       createEnable = true;
+      extraOptions =
+        [ "this message has been set  in the name of the holy father" ];
     })
+    # you can implement warnings to get the errors upon unexpected evalation results
+    /* {
+         warnings = if config.enable == config.createEnable then
+           [ "both vals msut evaluate to true" ]
+         else
+           [ ];
+       }
+    */
+    # you can use assertion for guaranteed behavoiur of the code by specifying where the code block might fail
     {
-      warnings = if config.enable == config.createEnable then
-        [ "both vals msut evaluate to true" ]
-      else
-        [ ];
+      assertions = [{
+
+        assertion = config.enable != config.createEnable;
+        message = "either of the configuration is not  enabled";
+
+      }];
     }
+
   ];
 
 }
