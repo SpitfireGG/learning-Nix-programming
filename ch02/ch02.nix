c7376b2658c172f5807e51549aa35bc56e8f8e60
SpitfireGG <raambasnet02@gmail.com>
2025-04-30 22:07:16 +0545

    add: string interpolation(url) & recursive sets

--- ch02/ch02.nix
+++ ch02/ch02.nix
@@ -1,44 +1,66 @@
-#sets
+## sets
+#############################
 
-# NOTE: comment one of the following codes and run for yourself to find the output and tinker the code to see what happens
+# There is so much more to learn about sets and we'll cover them in later chapters when we  discuss avout `builtins and  lib`                     comment out one of the sections and run the desired section, then run the other one by commenting the current one and vice versa.
 
-# comment or uncomment one of the section and play around until you feel confident and move onto the next section
+## section 1
+/*
+  let
+    # the attribute name 'name' in this case doesnot have to be inside the quotes but the value needs to be, Also when assigned an attribute with a value it must end with ';'
+    package = {
+      name = "git";
+      version = "1.12.1";
+      release = 2002;
+      sha256 = "35vj35j5vb5jvb4j54545b42jh5b"; # this is typed randomly btw
+    };
+    # also the end of the list must also have ';' in the end
+    description = ''
+      testing!!!! this is not true btw
+      the package ${package.name} was introduced in the late ${toString package.release}
+    '';
+  in
+  # the values can be captured using the dot notation
+  description
+*/
 
-# section 1
-let
-  person = {
-    name = "scoop"; # the attribute name 'name' in this case doesnot have to be inside the quotes but the value needs to be, Also when assigned an attribute with a value it must end with ';'
-    age = 10;
-    address = "kathmandu, Nepal";
-  }; # also the end of the list must also have ';' in the end
-in
-person.address
-  # run with nix eval --file ch02.nix
+# run with nix eval --file ch02.nix
 
-  # section 2
-  /*
-    let
-      services = {
-        name = "ssh";
-        description = "secure shell for securely connecting to remote servers";
-        # this is a nested set attribute
-        settings = {
-          enable = true;
-          params = "<some parameters>";
-          auto_enable_ssh_daemon = false;
-        };
-      };
-    in
-    services.settings.auto_enable_ssh_daemon
-  */
+## section 2
+# Recursive sets
 
-  # section 3
-  # Recursive sets
-  # Unlike the ordinary set, the recursive set which allows the attributes from within the set without any further let-in constructs
+/*
   rec {
     x = 1;
     y = x + 1;
     z = y + 1;
   }
+*/
 
-#section 4
+## section 3
+let
+  services = {
+    name = "random service";
+    port = 8080;
+    description = "secure shell for securely connecting to remote servers";
+    # this is a nested set attribute
+
+    settings = rec {
+      # using the rec keyword lets us use the attributes from within the same attribute, more about it on later chapters
+      enable = true;
+      enable_by_default = false;
+      params = "--help";
+
+      description =
+        let
+          e = enable;
+          serviceName = services.name;
+          serviceport = services.port;
+        in
+        "the url of the service is ${
+          if e then "https://${serviceName}:${toString serviceport}" else "service is not running"
+        }";
+
+    };
+  };
+in
+services.settings.description
