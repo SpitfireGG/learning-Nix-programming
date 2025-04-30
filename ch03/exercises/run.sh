44d1dc38f0a76286656b864449ae0d60eb9f63b2
SpitfireGG <raambasnet02@gmail.com>
2025-04-30 23:49:14 +0545

    add: nix evaluator script

---
+++ ch03/exercises/run.sh
@@ -0,0 +1,28 @@
+#!/run/current-system/sw/bin/bash
+
+set -e
+
+echo "";
+echo "                                    Check assertions                                ";
+echo "       the nix files must all result to provided output for all the assertions to pass";
+echo "";
+echo "running all the *.nix files in the exercises directory";
+
+for i in {1..6}; do
+    ex_f=$(printf "ex%03d.nix" "$i");
+    if [ -f "${ex_f}" ]; then
+        echo "running ${ex_f}......";
+        nix eval --impure --file "./${ex_f}"
+        echo "OK (Assertion passed)";
+        else
+            echo "skipping.... file ${ex_f} not found!";
+    fi
+done
+
+echo ""
+echo "exercises directory -> assertions passed"
+
+stop
+
+
+
